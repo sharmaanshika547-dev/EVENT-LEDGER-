bdedb193d17ef82482605399decbf601c6d40e10
Anshika Sharma <sharmaanshika547@gmail.com>
2026-03-19 18:07:32 +0530

    feat:implement update event endpoint with validation

--- app/routes/events.py
+++ app/routes/events.py
@@ -11,10 +11,20 @@ event_id=1
 def create_events():
 	global event_id
 	data = request.get_json()
-	events[event_id]=data
+        
+    if not data:
+        return {"error":"no data"},404
+    if "title" not in data["title"]:
+        return{"error":"no title"},
+    if not data["title"]:
+        return {"error":"title must be string"}]
+    if not isinstance(data["title"],str:
+         return{"error":"data type not string "},400 
+    
+	events[event_id]=data["title"]
 	response={
 		"id":event_id,
-		"event":data
+		"title":data["title"]
 	}
 	event_id+=1
 
@@ -26,17 +36,28 @@ def get_events():
 
 @events_bp.route("/events/<int:id>", methods=["DELETE"])
 def delete_event(id):
+
     if id in events:
         del events[id]
-        return {"message": "Event deleted"}, 200
+        return {"message": f"Event {id} deleted"}, 204
     else:
         return {"error": "Event not found"}, 404
 
 @events_bp.route("/events/<int:id>", methods=["PUT"])
 def update_event(id):
-    if id in events:
-        data = request.get_json()
-        events[id] = data
+    if id not in events:
+        return error{"error":"event not found"},404
+    data=request.get_json()
+    if not data:
+        return {"error":"no data provided"},400
+    if "title" not in data:
+        return {"error":"title required"},400
+    if not data["title"]:
+        return {"error":"title cant be empty"},400
+    if not isisntance(data["title",str]):
+        return {"error":"title must be a string"},400
+
+        events[id] = data["title"]
         return {
             "id": id,
             "event": events[id]
